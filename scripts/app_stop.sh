415c9d42a4dba8a968c6d8c44b4f99d28907ce2a
carson-chadwick <156139394+carson-chadwick@users.noreply.github.com>
2026-01-14 17:34:52 -0700

    Enhance app_stop.sh with error handling
    Added error handling to stop and delete app safely.

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd /home/ec2-user/app
-export NVM_DIR="/home/ec2-user/.nvm" 
+set -e
+
+export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-pm2 stop index.js
+
+# Stop/delete app if it exists, otherwise do nothing
+pm2 stop index.js || true
+pm2 delete index.js || true
+
