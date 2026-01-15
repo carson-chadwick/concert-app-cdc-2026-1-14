86b89a07eaf5f8d11e3fa831f86282b65b7b3dee
carson-chadwick <156139394+carson-chadwick@users.noreply.github.com>
2026-01-14 17:39:26 -0700

    Modify app_stop.sh to handle errors silently
    Suppress error messages when stopping or deleting the app.

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -e
 
 export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-# Stop/delete app if it exists, otherwise do nothing
-pm2 stop index.js || true
-pm2 delete index.js || true
+pm2 stop index.js 2>/dev/null || true
+pm2 delete index.js 2>/dev/null || true
+
+exit 0
+
 
